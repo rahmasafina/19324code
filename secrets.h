c23f480325d29de7430216ec348392e81e898337
rahmasafina <104700866+rahmasafina@users.noreply.github.com>
2022-12-11 11:27:57 +0200

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,8 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+#define SECRET_SSID "Sufsuf"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "sufsusf 2021"	// replace MyPassword with your WiFi password
+
+#define SECRET_CH_ID  1975019
+#define SECRET_WRITE_APIKEY "OA50B9QU4ODBJ6SO"   // replace XYZ with your channel write API Key
